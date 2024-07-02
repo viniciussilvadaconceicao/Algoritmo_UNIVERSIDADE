deaa2a360d62737f02479546c356d8fa7bf1a96c
VINICIUS SILVA <ra202323388@univassouras.edu.br>
2024-07-02 20:02:08 -0300

    Create exerc3.py

---
+++ p2/exerc3.py
@@ -0,0 +1,12 @@
+'''faça um programa que leia 8 numeros e informe a soma e a media dos numeros'''
+numeros = list()
+soma = 0
+cont = 0
+for i in range(8):
+    num = int(input(f'Digite o {i+1}° numero: '))
+    numeros.append(num)
+    soma += num
+    cont += 1
+    media = soma / cont
+    
+print(f'a soma desses numeros digitados da {soma} e a media é {media}')
